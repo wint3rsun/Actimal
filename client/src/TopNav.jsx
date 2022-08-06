30d446352622fa17754179728d9d22fa8c9c2369
Abigail Addae <abigail2addae@gmail.com>
2022-08-06 13:32:48 -0700

    change class to className

--- client/src/TopNav.jsx
+++ client/src/TopNav.jsx
@@ -3,30 +3,30 @@ import "./topNav.scss";
 export default function TopNav() {
   return (
 
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Final !</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Final !</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-            <a href="/profile"><i class="fa fa-fw fa-user" aria-hidden="true"></i> My Profile</a>
+        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+            <a href="/profile"><i className="fa fa-fw fa-user" aria-hidden="true"></i> My Profile</a>
             </li>
-            <li class="nav-item">
-            <a href="/dashboard"><i class="fa fa-tasks"></i> Dashboard</a>
+            <li className="nav-item">
+            <a href="/dashboard"><i className="fa fa-tasks"></i> Dashboard</a>
             </li>
-            <li class="nav-item">
-            <a href="/myChallenges"><i class="fa fa-bullseye"></i> My Challenges</a>
+            <li className="nav-item">
+            <a href="/myChallenges"><i className="fa fa-bullseye"></i> My Challenges</a>
             </li>
-            <li class="nav-item">
-            <a href="/myPets"><i class="fa fa-paw"></i> My Pets</a>
+            <li className="nav-item">
+            <a href="/myPets"><i className="fa fa-paw"></i> My Pets</a>
             </li>
           </ul>
         </div>
-        <ul class="nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/"><i class="fa fa-sign-out"></i> Logout</a></li>
+        <ul className="nav justify-content-end">
+            <li className="nav-item"><a className="nav-link" href="/"><i className="fa fa-sign-out"></i> Logout</a></li>
           </ul>
       </div>
     </nav>

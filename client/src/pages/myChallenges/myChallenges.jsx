7619da6888cb0c688b84f5c11ffbdff8efdf944b
Abigail Addae <abigail2addae@gmail.com>
2022-08-06 12:35:56 -0700

    add mode state and update show function

--- client/src/pages/myChallenges/myChallenges.jsx
+++ client/src/pages/myChallenges/myChallenges.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 
 import "./myChallenges.scss";
 import ChallengeListItem from "../../ChallengeListItem";
 import QuickStats from "../../QuickStats";
 import TopNav from "../../TopNav";
 import Footer from "../../Footer";
+import Challenge from "../../Challenge";
 
+
+const SHOW_ALL = "SHOW_ALL";
+const SHOW_ONE = "SHOW_ONE";
 
 export default function MyChallenges() {
+  const [mode, setMode] = useState(SHOW_ALL);
 
   const show = () => {
-    alert("clicked show");
+    setMode(SHOW_ONE);
   }
 
   const join = () => {
@@ -20,20 +26,27 @@ export default function MyChallenges() {
     
     <div>
       <TopNav/>
-      <div className="d-flex flex-row justify-content-between border">
-        
-        <div className="mx-5 my-5">
-          <QuickStats />
+
+      {
+      mode === SHOW_ALL && (
+        <div className="d-flex flex-row justify-content-between border">
+          <div className="mx-5 my-5">
+            <QuickStats />
+          </div>
+
+          <div className="mx-5 my-5">
+            <ChallengeListItem onShow={show} onJoin={join}/>
+            <ChallengeListItem  onShow={show} onJoin={join}/>
+            <ChallengeListItem  onShow={show} onJoin={join}/>
+          </div>
         </div>
-
-        <div className="mx-5 my-5">
-          <ChallengeListItem onShow={show} onJoin={join}/>
-          <ChallengeListItem  onShow={show} onJoin={join}/>
-          <ChallengeListItem  onShow={show} onJoin={join}/>
-        </div>
-      </div>
-
-
+      )}
+      {
+        mode === SHOW_ONE && (
+          <div className="d-flex flex-row justify-content-between border">
+            <Challenge />
+          </div>
+      )}
 
       <Footer/>
     </div>

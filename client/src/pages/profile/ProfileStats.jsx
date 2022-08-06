9afc884e94938719212ecbbfb1e23e5fd5d1d35b
Abigail Addae <abigail2addae@gmail.com>
2022-08-06 07:20:03 -0700

    add basic layout

---
+++ client/src/pages/profile/ProfileStats.jsx
@@ -0,0 +1,9 @@
+export default function ProfileStats() {
+  return (
+    <div>
+      <p>Distance Walked: 10km</p>
+      <p>Start Date: July 2, 2022</p>
+      <p>Current Streak: 100 Days</p>
+    </div>
+  )
+}

7bb9ec70da8aecacbb40a2b9a9d7dadba0258cb3
Abigail Addae <abigail2addae@gmail.com>
2022-08-08 12:30:24 -0700

    change data type of avatar_url

--- server/db/schema/03_animals.sql
+++ server/db/schema/03_animals.sql
@@ -4,6 +4,6 @@ CREATE TABLE animals (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
-  avatar_url VARCHAR(255) NOT NULL,
+  avatar_url TEXT NOT NULL,
   required_level INTEGER NOT NULL
 )

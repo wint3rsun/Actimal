f2eb3499e8efb6fd31c7f70d57f66bbaa09c680f
Abigail Addae <abigail2addae@gmail.com>
2022-08-07 22:23:29 -0700

    remove detailButton class

--- client/src/Pet.jsx
+++ client/src/Pet.jsx
@@ -12,7 +12,7 @@ export default function Pet() {
           <h2 className="card-text text-center">some name</h2>
           <div className="d-flex justify-content-between align-items-center">
             <div className="btn-group">
-              <button onClick={() => setOpenDetail(true)} type="button" className="detailButton btn btn-sm btn-outline-secondary">View</button>
+              <button onClick={() => setOpenDetail(true)} type="button" className="btn btn-sm btn-outline-secondary">View</button>
             </div>
             <small className="text-muted">some info</small>
           </div>

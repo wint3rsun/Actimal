493659c5cc0df983897ec2da567c2ffa502ad7bc
Abigail Addae <abigail2addae@gmail.com>
2022-08-08 12:10:41 -0700

    add name to animals schema

--- server/db/schema/03_animals.sql
+++ server/db/schema/03_animals.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS animals CASCADE;
 
 CREATE TABLE animals (
   id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   avatar_url VARCHAR(255) NOT NULL,
   required_level INTEGER NOT NULL

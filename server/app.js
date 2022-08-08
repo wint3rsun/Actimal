b69e802c6778b0f316a342cdba69e8b96af268f0
Zuge Gao <audreygaozu@hotmail.com>
2022-08-08 23:51:03 +0000

    add routes

--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ const db = require('./configs/db.config');
 
 // var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var charactersRouter = require('./routes/characters');
+var challengesRouter = require('./routes/challenges');
+var animalsRouter = require('./routes/animals');
 
 var app = express();
 
@@ -19,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter());
+app.use('/users', usersRouter(db));
+app.use('/characters', charactersRouter(db));
+app.use('/challenges', challengesRouter(db));
+app.use('/animals', animalsRouter(db));
 
 
 

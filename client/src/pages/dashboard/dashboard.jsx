cafe2ff29d0bfc55bc988e13759a88ce2755d455
Abigail Addae <abigail2addae@gmail.com>
2022-08-08 20:33:03 -0700

    add challenge list items toSee scrolling behaviour

--- client/src/pages/dashboard/dashboard.jsx
+++ client/src/pages/dashboard/dashboard.jsx
@@ -6,24 +6,34 @@ import QuickStats from "../../QuickStats";
 
 
 export default function Dashboard() {
+  const challenge = {
+    id: 1,
+    start_date: "21-01-2022",
+    end_date: "2022-01-22T08:00:00.000Z",
+    progress: "0.00",
+    quest_id: 1,
+    name: "10k Steps",
+    description: "Lets get to 10k steps!",
+    img_url: "https://img.freepik.com/free-vector/moving-forward-concept-illustration_114360-1641.jpg?w=1380&t=st=1659989244~exp=1659989844~hmac=271b63c1bc1e3e3e4a25bc140b94522552b2afcde7fa60996ab5876c22f43017",
+    goal: 10000,
+    goal_units: "steps",
+    required_level: 0,
+    base_experience: 10,
+    first_place_exp_bonus: 5,
+    second_place_exp_bonus: 3,
+    third_place_exp_bonus: 1
+  }
 
-    const challenge =     {
-      id: 1,
-      start_date: "21-01-2022",
-      end_date: "2022-01-22T08:00:00.000Z",
-      progress: "0.00",
-      quest_id: 1,
-      name: "10k Steps",
-      description: "Lets get to 10k steps!",
-      img_url: "https://img.freepik.com/free-vector/moving-forward-concept-illustration_114360-1641.jpg?w=1380&t=st=1659989244~exp=1659989844~hmac=271b63c1bc1e3e3e4a25bc140b94522552b2afcde7fa60996ab5876c22f43017",
-      goal: 10000,
-      goal_units: "steps",
-      required_level: 0,
-      base_experience: 10,
-      first_place_exp_bonus: 5,
-      second_place_exp_bonus: 3,
-      third_place_exp_bonus: 1
-      }
+  const user = {
+    id: 1,
+    username: "username1",
+    password: "password",
+    email: "username1@gmail.com",
+    experience_points: 10,
+    levels: 1,
+    character_id: 1
+  }
+
 
   return (
     <div>
@@ -36,10 +46,12 @@ export default function Dashboard() {
 
         <div className="mx-5 my-5">
           <ChallengeListItem challenge={challenge} />
+          <ChallengeListItem challenge={challenge} />
+          <ChallengeListItem challenge={challenge} />
+          <ChallengeListItem challenge={challenge} />
+          <ChallengeListItem challenge={challenge} />
         </div>
       </div>
-
-
 
       <Footer/>
     </div>

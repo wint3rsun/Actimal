b5654e2fb371fdb089cc9560d80e04c6b4e1225d
Abigail Addae <abigail2addae@gmail.com>
2022-08-08 19:27:04 -0700

    add route for challenges and associated quest

--- server/routes/challenges.js
+++ server/routes/challenges.js
@@ -8,6 +8,19 @@ module.exports = (db) => {
       res.json(data.rows);
     })
   });
+  router.get('/', (req, res) => {
+    const command = "SELECT * FROM game_challenges";
+    db.query(command).then(data => {
+      res.json(data.rows);
+    })
+  });
+
+  router.get('/quests', (req, res) => {
+    const command = "SELECT game_challenges.*, to_char(game_challenges.start_date, 'DD-MM-YYYY'), quests.* FROM game_challenges JOIN quests ON game_challenges.quest_id = quests.id";
+    db.query(command).then(data => {
+      res.json(data.rows);
+    })
+  });
 
   return router;
 }

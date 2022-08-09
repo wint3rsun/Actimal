81134eef529802fe4d0bcba70735c5e519cf591d
Abigail Addae <abigail2addae@gmail.com>
2022-08-08 22:21:09 -0700

    remove profile stats and use quick stats

--- client/src/pages/profile/ProfileFooter.jsx
+++ client/src/pages/profile/ProfileFooter.jsx
@@ -1,12 +1,11 @@
 import ProgressBar from "./ProgressBar"
-import ProfileStats from "./ProfileStats"
+import QuickStats from "../../QuickStats"
 
 export default function ProfileFooter() {
   return (
     <footer className="d-flex flex-row justify-content-between align-items-center">
-      <p className="d-flex flex-column">Level <span className="mx-auto">1</span></p>
       <ProgressBar />
-      <ProfileStats />
+      <QuickStats />
     </footer>
   )
 }
--- client/src/pages/profile/ProfileStats.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProfileStats() {
-  return (
-    <div>
-      <p>Distance Walked: 10km</p>
-      <p>Start Date: July 2, 2022</p>
-      <p>Current Streak: 100 Days</p>
-    </div>
-  )
-}

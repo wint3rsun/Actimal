a30150278878483524e9506c8f708370a37e1add
Abigail Addae <abigail2addae@gmail.com>
2022-08-08 22:22:12 -0700

    add mock prop data

--- client/src/pages/profile/ProgressBar.jsx
+++ client/src/pages/profile/ProgressBar.jsx
@@ -1,12 +1,18 @@
 import "./ProgressBar.scss"
 
 export default function ProgressBar() {
+  const props = {
+    min: 0,
+    max: 100,
+    current: 10,
+    unit: 'EXP'
+  }
   return (
     <div className="progress-container d-flex flex-column">
       <div className="progress">
-        <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style={{width:"40%"}}></div>
+        <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow={props.current} aria-valuemin={props.min} aria-valuemax={props.max} style={{width:`${props.current/props.max*100}%`}}></div>
       </div>
-      <p>EXP: 400 / 1000</p>
+      <p>{`${props.unit}: ${props.current} / ${props.max}`}</p>
     </div>
   )
 }

17427333440aa50b22687b0fa75949a6d72249f8
Abigail Addae <abigail2addae@gmail.com>
2022-08-08 19:27:20 -0700

    update to use props

--- client/src/ChallengeListItem.jsx
+++ client/src/ChallengeListItem.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
+import "./ChallengeListItem.scss"
 
 export default function ChallengeListItem(props) {
 
   return (
-    <section className="d-flex flex-row">
-    <img src="https://placekitten.com/200/139" alt="challenge" />
-    <li className="card">
-      <div className="card-body">
-        <h5 className="card-title">Challenge Name</h5>
-        <p className="card-text">Challenge Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non ultrices ipsum, at rutrum lorem. Phasellus orci metus, lobortis a rutrum in, venenatis nec lorem. Duis a nisl gravida, luctus tortor aliquam, consequat enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi rutrum ac dolor in ornare. Proin non sollicitudin eros. Nunc ante dolor, cursus a nunc id, eleifend pulvinar dolor. Maecenas arcu tellus, luctus eget egestas et, convallis quis velit. Aliquam auctor, odio sed sollicitudin bibendum, elit libero rhoncus velit, sed maximus mauris purus et felis. Morbi ipsum neque, commodo egestas lacinia lacinia, iaculis eget massa.</p>
-        <p className="card-text">Start Date: July 1st, 2022</p>
-        <div className="d-flex flex-row justify-content-end">
-          <a onClick={props.onJoin} className="btn btn-primary mx-2">Join</a>
-          <a onClick={props.onShow} className="btn btn-primary mx-2">See Details</a>
+    <section className="challenge-list-section d-flex flex-row">
+      <img className="challenge-list-img" src={props.challenge.img_url}alt="challenge" />
+      <li className="card">
+        <div className="card-body">
+          <h5 className="card-title">{props.challenge.name}</h5>
+          <p className="card-text">{props.challenge.description}</p>
+          <p className="card-text">{`Start Date: ${props.challenge.start_date}`}</p>
+          <div className="d-flex flex-row justify-content-end">
+            {props.isJoined && <a onClick={props.onJoin} className="btn btn-primary mx-2">Join</a>}
+            <a onClick={props.onShow} className="btn btn-primary mx-2">See Details</a>
+          </div>
+          <br />
         </div>
-        <br />
-      </div>
-    </li>
+      </li>
     </section>
-  )
-
+  );
 }

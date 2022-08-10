813b2c4023710d24d460424a6e099320516ab240
Abigail Addae <abigail2addae@gmail.com>
2022-08-09 19:17:54 -0700

    update route to return human readable date

--- server/routes/challenges.js
+++ server/routes/challenges.js
@@ -3,22 +3,33 @@ const router = require('express').Router();
 module.exports = (db) => {
   // all routes will go here 
   router.get('/', (req, res) => {
-    const command = "SELECT * FROM game_challenges";
-    db.query(command).then(data => {
-      res.json(data.rows);
-    })
-  });
-  router.get('/', (req, res) => {
-    const command = "SELECT * FROM game_challenges";
+    const command = "SELECT id, to_char(start_date, 'DD-MM-YYYY') AS start_date, to_char(end_date, 'DD-MM-YYYY') AS end_date, progress, quest_id FROM game_challenges ORDER BY start_date";
     db.query(command).then(data => {
       res.json(data.rows);
     })
   });
 
+  // router.get('/', (req, res) => {
+  //   const command = "SELECT * FROM game_challenges";
+  //   db.query(command).then(data => {
+  //     res.json(data.rows);
+  //   })
+  // });
+
+  // router.get('/quests', (req, res) => {
+  //   const command = "SELECT game_challenges.*, to_char(game_challenges.start_date, 'DD-MM-YYYY'), quests.* FROM game_challenges JOIN quests ON game_challenges.quest_id = quests.id";
+  //   db.query(command).then(data => {
+  //     res.json(data.rows);
+  //   })
+  // });
   router.get('/quests', (req, res) => {
-    const command = "SELECT game_challenges.*, to_char(game_challenges.start_date, 'DD-MM-YYYY'), quests.* FROM game_challenges JOIN quests ON game_challenges.quest_id = quests.id";
+    const command = "SELECT * FROM quests";
     db.query(command).then(data => {
-      res.json(data.rows);
+      var quests = {};
+    data.rows.forEach(element => {
+      quests[`${element.id}`]= element;
+    });
+      res.json(quests);
     })
   });
 

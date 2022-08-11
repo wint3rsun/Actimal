505de85a4551291a01a12ffc38f3471e501ee7b6
Abigail Addae <abigail2addae@gmail.com>
2022-08-10 18:01:43 -0700

    pass props

--- client/src/RankList.jsx
+++ client/src/RankList.jsx
@@ -4,17 +4,17 @@ import Top3 from "./Top3";
 
 import "./RankList.scss"
 
-export default function RankList({participants, characters}) {
+export default function RankList({participants, characters, max, unit}) {
 
   const bottomList = participants.bottom.map((participant, index) => {
     return (
-    <RankListItem key={index} position={index + 4} user={participant} character={characters[participant.character_id]} />
+    <RankListItem key={index} position={index + 4} user={participant} character={characters[participant.character_id]} unit={unit} max={max} />
     );
   });
 
   const topList = participants.top3.map((participant, index) => {
     return (
-      <Top3 key={index} position={index + 1} user={participant} character={characters[participant.character_id]} />
+      <Top3 key={index} position={index + 1} user={participant} character={characters[participant.character_id]} unit={unit} max={max} />
     )
   });
   
--- client/src/RankListItem.jsx
+++ client/src/RankListItem.jsx
@@ -1,12 +1,12 @@
 import ProgressBar from "./pages/profile/ProgressBar";
 
-export default function RankListItem({user, character, position}) {
+export default function RankListItem({user, character, position, unit, max}) {
   return (
     <div className="item">
       <p className="position">{position}</p>
       <img src={character.avatar_url} alt={`player ${user.username}'s avatar`} className="avatar" />
       <p className="username">{user.username}</p>
-      <ProgressBar />
+      <ProgressBar unit={unit} max={max} current={user.progress} />
     </div>
   )
 }

b2d897bafca675e768bec7edde448f07fc9119ae
Abigail Addae <abigail2addae@gmail.com>
2022-08-10 17:18:22 -0700

    update to pass prop data to children dynamically

--- client/src/RankList.jsx
+++ client/src/RankList.jsx
@@ -1,66 +1,31 @@
 import ProgressBar from "./pages/profile/ProgressBar";
 import RankListItem from "./RankListItem";
+import Top3 from "./Top3";
 
 import "./RankList.scss"
 
-export default function RankList(props) {
-  const participants = [
-      {
-      id: 1,
-      progress: "20.3",
-      game_challenges_id: 1,
-      users_id: {
-        username: "username1",
-        avatar_url: "https://robohash.org/340c525dec61deba2666cb58c149840e?set=set1&bgset=&size=400x400",
-      }
-      },
-      {
-      id: 3,
-      progress: "50.0",
-      game_challenges_id: 1,
-      users_id: {
-        username: "username2",
-        avatar_url: "https://robohash.org/340c525dec61deba2666cb58c149840e?set=set2&bgset=&size=400x400"
-      }
-      },
-      {
-      id: 9,
-      progress: "0.00",
-      game_challenges_id: 1,
-      users_id: {
-        username: "username5",
-        avatar_url: "https://robohash.org/340c525dec61deba2666cb58c149840e?set=set2&bgset=&size=400x400"
-      }
-      }
-    ]
+export default function RankList({participants, characters}) {
+
+  const bottomList = participants.bottom.map((participant, index) => {
+    return (
+    <RankListItem key={index} position={index + 4} user={participant} character={characters[participant.character_id]} />
+    );
+  });
+
+  const topList = participants.top3.map((participant, index) => {
+    return (
+      <Top3 key={index} position={index + 1} user={participant} character={characters[participant.character_id]} />
+    )
+  });
+  
+
   return (
     <div className="ranking-container mx-5">
       <div className="top3">
-        <div className="second item">
-          <p className="position">2</p>
-          <img src={participants[1].users_id.avatar_url} alt={`${participants[1].users_id.username}'s avatar`} className="avatar" />
-          <p className="username">{participants[1].users_id.username}</p>
-          <ProgressBar />
-        </div>
-        <div className="first item">
-        <img src={participants[0].users_id.avatar_url} alt={`${participants[0].users_id.username}'s avatar`} className="avatar" />
-          <p className="username">{participants[0].users_id.username}</p>
-          <p className="username">WinterMoon</p>
-          <ProgressBar />
-        </div>
-        <div className="third item">
-          <p className="position">3</p>
-          <img src={participants[2].users_id.avatar_url} alt={`${participants[2].users_id.username}'s avatar`} className="avatar" />
-          <p className="username">{participants[2].users_id.username}</p>
-          <ProgressBar />
-        </div>
-      </div>
+        { topList }
+      </div> 
       <div className="rank-list">
-
-        <RankListItem position={4}/>
-        <RankListItem position={5}/>
-        <RankListItem position={6}/>
-        <RankListItem position={7}/>
+        { bottomList }
       </div>
     </div>
   )

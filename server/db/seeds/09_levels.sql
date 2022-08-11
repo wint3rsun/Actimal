d53608f3f9794506adb499c8883111e9fa90d5bf
Abigail Addae <abigail2addae@gmail.com>
2022-08-10 20:10:29 -0700

    add seeds for levels table

---
+++ server/db/seeds/09_levels.sql
@@ -0,0 +1,19 @@
+INSERT INTO levels (id, min, max) VALUES (0, 0, 49.99);
+INSERT INTO levels (min, max) VALUES (50, 99.99);
+INSERT INTO levels (min, max) VALUES (100, 199.99);
+INSERT INTO levels (min, max) VALUES (200, 299.99);
+INSERT INTO levels (min, max) VALUES (300, 399.99);
+INSERT INTO levels (min, max) VALUES (400, 499.99);
+INSERT INTO levels (min, max) VALUES (500, 599.99);
+INSERT INTO levels (min, max) VALUES (600, 699.99);
+INSERT INTO levels (min, max) VALUES (700, 799.99);
+INSERT INTO levels (min, max) VALUES (800, 899.99);
+INSERT INTO levels (min, max) VALUES (900, 999.99);
+INSERT INTO levels (min, max) VALUES (1000, 1499.99);
+INSERT INTO levels (min, max) VALUES (1500, 1999.99);
+INSERT INTO levels (min, max) VALUES (2000, 2499.99);
+INSERT INTO levels (min, max) VALUES (3000, 3499.99);
+INSERT INTO levels (min, max) VALUES (3500, 3999.99);
+INSERT INTO levels (min, max) VALUES (4000, 4499.99);
+INSERT INTO levels (min, max) VALUES (5000, 5499.99);
+

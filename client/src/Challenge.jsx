468f75221b97dfa6ca81665f440a4a4cbdea1d71
Abigail Addae <abigail2addae@gmail.com>
2022-08-10 17:19:17 -0700

    add hook for challenge participants

--- client/src/Challenge.jsx
+++ client/src/Challenge.jsx
@@ -1,32 +1,55 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import ProgressBar from "./pages/profile/ProgressBar"
-import RankList from "./RankList";
-import io from 'socket.io-client';
+import axios from "axios";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage } from "@fortawesome/free-solid-svg-icons";
+
+import ProgressBar from "./pages/profile/ProgressBar"
+import RankList from "./RankList";
 import Chat from "./Chat";
 
 import "./Challenge.scss"
 
-
-
-export default function Challenge(props) {
+export default function Challenge({challenge, quest, characters}) {
+  //for close button navigation
   const navigate = useNavigate();
   
-  
+  const [participants, setParticipants] = useState({
+    top3: [],
+    bottom: []
+  });
+
+  useEffect(() => {
+    const participantsURL = `http://localhost:8080/participants/${challenge.id}`;
+
+    axios.get(participantsURL)
+    .then((res) => {
+      const top3 = res.data.filter((p, index) => {if (index < 3) return p});
+      const participants = res.data.filter((p,index) => {if (index >= 3) return p});
+      setParticipants({
+        top3: top3,
+        bottom: participants
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  }, []);
+
 
   return (
     <main className="challenge position-relative">
       <button type="button" className="btn-close position-absolute" aria-label="Close" onClick={() => navigate(0)}></button>
       <section className="challenge-header mt-5">
-        <h1>I am a Challenge!</h1>
+        <h1>{quest.name}</h1>
         <h2>Leaderboard</h2>
         <ProgressBar />
       </section>
 
       <section className="position-relative">
-        <RankList />
+        <RankList participants={participants} characters={characters}/>
       </section>
 
       <section className="d-flex flex-row-reverse mb-3">

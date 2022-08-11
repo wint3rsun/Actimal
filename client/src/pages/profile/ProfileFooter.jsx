c6ef985129b8ae6ba070f9ec177e5772f89a95a7
Abigail Addae <abigail2addae@gmail.com>
2022-08-11 14:33:25 -0700

    update profile footer to use props

--- client/src/pages/profile/ProfileFooter.jsx
+++ client/src/pages/profile/ProfileFooter.jsx
@@ -1,10 +1,10 @@
 import ProgressBar from "./ProgressBar"
 import QuickStats from "../../QuickStats"
 
-export default function ProfileFooter({user}) {
+export default function ProfileFooter({user, level}) {
   return (
     <footer className="d-flex flex-row justify-content-between align-items-center">
-      <ProgressBar current={user.experience_points} />
+      <ProgressBar current={user.experience} min={level.min} max={level.max}/>
       <QuickStats user={user} />
     </footer>
   )

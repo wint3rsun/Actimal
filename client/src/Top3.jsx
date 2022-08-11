7abfc6df5e587690e6ba47fb927d6d49aace8b82
Abigail Addae <abigail2addae@gmail.com>
2022-08-10 18:03:57 -0700

    pass props to progressbar

--- client/src/Top3.jsx
+++ client/src/Top3.jsx
@@ -4,7 +4,7 @@ const FIRST = "first order-2";
 const SECOND = "second order-1";
 const THIRD = "third order-3";
 
-export default function Top3({user, position, character}) {
+export default function Top3({user, position, character, unit, max}) {
   let placeClass = "";
   if(position === 1) placeClass = FIRST;
   if(position === 2) placeClass = SECOND;
@@ -15,7 +15,7 @@ export default function Top3({user, position, character}) {
       <p className="position">{position}</p>
       <img src={character.avatar_url} alt={`${user.username}'s avatar`} className="avatar" />
       <p className="username">{user.username}</p>
-      <ProgressBar />
+      <ProgressBar unit={unit} max={max} current={user.progress} />
     </div>
   )
 }

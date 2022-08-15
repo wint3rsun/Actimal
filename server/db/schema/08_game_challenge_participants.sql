45a6270035b0854f332e03f70b56e6e9fc786c78
Abigail Addae <abigail2addae@gmail.com>
2022-08-15 13:55:52 -0700

    add isComplete column

--- server/db/schema/08_game_challenge_participants.sql
+++ server/db/schema/08_game_challenge_participants.sql
@@ -3,5 +3,6 @@ CREATE TABLE game_challenge_participants (
   id SERIAL PRIMARY KEY NOT NULL,
   progress numeric NOT NULL DEFAULT 0.00,
   game_challenges_id integer REFERENCES game_challenges(id) ON DELETE CASCADE NOT NULL,
-  users_id integer REFERENCES users(id) ON DELETE CASCADE NOT NULL
+  users_id integer REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  isComplete BOOLEAN DEFAULT false
 );

3bf2c79191c5bbf491f41469e32c542b2b26ace7
Abigail Addae <abigail2addae@gmail.com>
2022-08-16 14:53:39 -0700

    add back progress column

--- server/db/schema/07_game_challenges.sql
+++ server/db/schema/07_game_challenges.sql
@@ -3,6 +3,7 @@ CREATE TABLE game_challenges (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL DEFAULT CURRENT_DATE,
   end_date DATE NOT NULL DEFAULT CURRENT_DATE,
+  progress numeric NOT NULL DEFAULT 0.00,
   quest_id integer REFERENCES quests(id) ON DELETE CASCADE NOT NULL,
   challenges_type VARCHAR(255) NOT NULL
 );

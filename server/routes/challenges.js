7dfad530e8d60bab086d4ece5f19ccff4af27c30
Abigail Addae <abigail2addae@gmail.com>
2022-08-16 09:39:29 -0700

    update query to select progress column

--- server/routes/challenges.js
+++ server/routes/challenges.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 module.exports = (db) => {
   // all routes will go here 
   router.get('/', (req, res) => {
-    const command = "SELECT id, challenges_type ,to_char(start_date, 'DD-MM-YYYY') AS start_date, to_char(end_date, 'DD-MM-YYYY') AS end_date, progress, quest_id FROM game_challenges ORDER BY start_date";
+    const command = "SELECT id, challenges_type ,to_char(start_date, 'DD-MM-YYYY') AS start_date, to_char(end_date, 'DD-MM-YYYY') AS end_date, quest_id FROM game_challenges ORDER BY start_date";
     db.query(command).then(data => {
       res.json(data.rows);
     })
